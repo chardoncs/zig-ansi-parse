5195f4445ae1f9d1dc7678879a88db5caf91168d
Charles Dong <chardon_cs@proton.me>
2024-11-02 13:51:56 +0800

    Fix typing errors and make it runnable

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 const Allocator = std.mem.Allocator;
-const ArrayList = std.ArrayList;
 
 const ESC = '\x1b';
 const CSI = '[';
@@ -111,153 +110,217 @@ fn isInteger(input: []const u8) bool {
     return true;
 }
 
-/// Parse a format string to ANSI-escapable
-pub fn parse(allocator: Allocator, input: []const u8) Allocator.Error![]const u8 {
-    const output = try ArrayList(u8).initCapacity(allocator, input.len * 1.5);
+fn append(comptime T: type) fn (*T, *usize, u8) void {
+    return comptime struct {
+        fn f(arr: *T, cursor: *usize, ch: u8) void {
+            if (cursor.* >= arr.len) {
+                return cursor;
+            }
 
-    var i: usize = 0;
-    var ch: u8 = undefined;
-    var tag = [171:0]u8{};
-    var split_arr = [10]usize{};
-
-    outer_parse_loop: while (i < input.len) {
-        ch = input[i];
-
-        switch (ch) {
-            '<' => {
-                // Tag start
-                var j = i + 1;
-                switch (input[j]) {
-                    '!' => {
-                        // Behavioral
-                        j += 1;
-                        var times: ?u32 = null;
-                        var cursor: usize = 0;
-                        var ch2: u8 = undefined;
-                        var parse_times = false;
-
-                        while (j < input.len) {
-                            ch2 = input[j];
-
-                            switch (ch2) {
-                                '*' => {
-                                    parse_times = true;
-                                },
-
-                                '>' => {
-                                    tag[cursor] = 0;
-                                    const code = behavioral_code_map.get(tag[0..cursor]) orelse break;
-
-                                    try output.appendNTimes(code, @intCast(times orelse 1));
-                                    i = j;
-                                    ch = ch2;
-                                    break;
-                                },
-
-                                else => {
-                                    if (parse_times) {
-                                        times = (times orelse 0) * 10 + @as(u32, @intCast(ch2));
-                                    } else if (cursor < tag.len) {
-                                        tag[cursor] = ch2;
-                                        cursor += 1;
-                                    }
-                                },
-                            }
-
-                            j += 1;
-                        }
-                    },
-                    else => {
-                        // ANSI
-                        var is_closed = false;
-                        var ch2: u8 = undefined;
-                        var cursor: usize = 0;
-                        var split_size: usize = 0;
-
-                        while (j < input.len and cursor < tag.len) {
-                            ch2 = input[j];
-
-                            switch (ch2) {
-                                '>' => {
-                                    // Tag end
-                                    is_closed = true;
-                                    tag[cursor] = 0;
-                                    break;
-                                },
-
-                                else => {
-                                    if (ch2 == ';' and split_size < split_arr.len) {
-                                        split_arr[split_size] = ch2;
-                                        split_size += 1;
-                                    }
-
-                                    tag[cursor] = ch2;
-                                    cursor += 1;
-                                },
-                            }
-
-                            j += 1;
-                        }
-
-                        if (is_closed) {
-                            try output.appendSlice(.{ ESC, CSI });
-
-                            var prev: usize = 0;
-
-                            for (split_arr) |delim_i| {
-                                const tag_piece = tag[prev..delim_i];
-                                try output.appendSlice(if (isInteger(tag_piece)) tag_piece else .{ansi_code_map.get(tag_piece)} orelse continue);
-                                prev = delim_i + 1;
-                            }
-
-                            const last_tag = tag[prev..];
-                            if (isInteger(last_tag)) {
-                                try output.appendSlice(last_tag);
-                            } else {
-                                if (ansi_code_map.get(last_tag)) |last_code| {
-                                    try output.append(last_code);
-                                }
-                            }
-
-                            try output.append('m');
-
-                            i = j;
-                            ch = ch2;
-                        } else {
-                            try output.append(ch);
-                        }
-                    },
-                }
-            },
-
-            '\\' => {
-                // Escape
-                const j = i + 1;
-                if (j >= input.len) {
-                    try output.append(ch);
-                    break :outer_parse_loop;
-                }
-
-                switch (input[j]) {
-                    '<', '>' => {
-                        i = j;
-                        ch = input[i];
-                        try output.append(ch);
-                    },
-                    else => {
-                        try output.append(ch);
-                    },
-                }
-            },
-
-            else => {
-                // Regular character
-                try output.append(ch);
-            },
+            arr.*[cursor.*] = ch;
+            cursor.* += 1;
         }
+    }.f;
+}
 
-        i += 1;
-    }
+fn appendMultiple(comptime T: type) fn (*T, *usize, []const u8) void {
+    return comptime struct {
+        fn f(arr: *T, cursor: *usize, chs: []const u8) void {
+            for (chs) |ch| {
+                if (cursor.* >= arr.len) {
+                    break;
+                }
 
-    return try output.toOwnedSlice();
+                arr.*[cursor.*] = ch;
+                cursor.* += 1;
+            }
+        }
+    }.f;
+}
+
+fn appendNTimes(comptime T: type) fn (*T, *usize, u8, u32) void {
+    return comptime struct {
+        fn f(arr: *T, cursor: *usize, ch: u8, times: u32) void {
+            for (0..times) |_| {
+                if (cursor.* >= arr.len) {
+                    break;
+                }
+
+                arr.*[cursor.*] = ch;
+                cursor.* += 1;
+            }
+        }
+    }.f;
+}
+
+/// Parse a format string to ANSI-escapable
+pub fn ComptimeParser(comptime input: []const u8) type {
+    const Out = [input.len * 4]u8;
+
+    return struct {
+        pub fn parse() [:0]const u8 {
+            comptime {
+                var output: Out = undefined; // TODO: Estimate capacity
+                var size: usize = 0;
+
+                var i: usize = 0;
+                var ch: u8 = undefined;
+                var tag: [171:0]u8 = undefined;
+                var split_arr: [10]usize = undefined;
+                var split_size: usize = 0;
+
+                outer_parse_loop: while (i < input.len and size < output.len) {
+                    ch = input[i];
+
+                    switch (ch) {
+                        '<' => {
+                            // Tag start
+                            var j = i + 1;
+                            switch (input[j]) {
+                                '!' => {
+                                    // Behavioral
+                                    j += 1;
+                                    var times: ?u32 = null;
+                                    var cursor: usize = 0;
+                                    var ch2: u8 = undefined;
+                                    var parse_times = false;
+
+                                    while (j < input.len) {
+                                        ch2 = input[j];
+
+                                        switch (ch2) {
+                                            '*' => {
+                                                parse_times = true;
+                                            },
+
+                                            '>' => {
+                                                tag[cursor] = 0;
+                                                const code = behavioral_code_map.get(tag[0..cursor]) orelse break;
+
+                                                for (0..times) |_| {
+                                                    if (size >= output.len) {
+                                                        break :outer_parse_loop;
+                                                    }
+
+                                                    output[size] = code;
+                                                    size += 1;
+                                                }
+
+                                                appendNTimes(Out)(&output, &size, ch2);
+
+                                                i = j;
+                                                ch = ch2;
+                                                break;
+                                            },
+
+                                            else => {
+                                                if (parse_times) {
+                                                    times = (times orelse 0) * 10 + @as(u32, @intCast(ch2));
+                                                } else if (cursor < tag.len) {
+                                                    tag[cursor] = ch2;
+                                                    cursor += 1;
+                                                }
+                                            },
+                                        }
+
+                                        j += 1;
+                                    }
+                                },
+                                else => {
+                                    // ANSI
+                                    var is_closed = false;
+                                    var ch2: u8 = undefined;
+                                    var cursor: usize = 0;
+                                    split_size = 0;
+
+                                    while (j < input.len and cursor < tag.len) {
+                                        ch2 = input[j];
+
+                                        switch (ch2) {
+                                            '>' => {
+                                                // Tag end
+                                                is_closed = true;
+                                                tag[cursor] = 0;
+                                                break;
+                                            },
+
+                                            else => {
+                                                if (ch2 == ';' and split_size < split_arr.len) {
+                                                    split_arr[split_size] = ch2;
+                                                    split_size += 1;
+                                                }
+
+                                                tag[cursor] = ch2;
+                                                cursor += 1;
+                                            },
+                                        }
+
+                                        j += 1;
+                                    }
+
+                                    if (is_closed) {
+                                        append(Out)(&output, &size, ESC);
+                                        append(Out)(&output, &size, CSI);
+
+                                        var prev: usize = 0;
+
+                                        for (0..split_size) |si| {
+                                            const tag_piece = tag[prev..split_arr[si]];
+                                            appendMultiple(Out)(&output, &size, if (isInteger(tag_piece)) tag_piece else [_]u8{ansi_code_map.get(tag_piece) orelse continue});
+                                            prev = split_arr[si] + 1;
+                                        }
+
+                                        const last_tag = tag[prev..];
+                                        if (isInteger(last_tag)) {
+                                            appendMultiple(Out)(&output, &size, last_tag);
+                                        } else {
+                                            if (ansi_code_map.get(last_tag)) |last_code| {
+                                                append(Out)(&output, &size, last_code);
+                                            }
+                                        }
+
+                                        append(Out)(&output, &size, 'm');
+
+                                        i = j;
+                                        ch = ch2;
+                                    } else {
+                                        append(Out)(&output, &size, ch);
+                                    }
+                                },
+                            }
+                        },
+
+                        '\\' => {
+                            // Escape
+                            const j = i + 1;
+                            if (j >= input.len) {
+                                append(Out)(&output, &size, ch);
+                                break :outer_parse_loop;
+                            }
+
+                            switch (input[j]) {
+                                '<', '>' => {
+                                    i = j;
+                                    ch = input[i];
+                                    append(Out)(&output, &size, ch);
+                                },
+                                else => {
+                                    append(Out)(&output, &size, ch);
+                                },
+                            }
+                        },
+
+                        else => {
+                            // Regular character
+                            append(Out)(&output, &size, ch);
+                        },
+                    }
+
+                    i += 1;
+                }
+
+                return (output[0..size].* ++ .{0})[0..size :0];
+            }
+        }
+    };
 }
